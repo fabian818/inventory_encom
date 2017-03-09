b91408ac62cdce870f3d49c81aa0c98fbc43fa73
Fabian Pierre Peña Jacobo <f.pena.jacobo@gmail.com>
2017-03-09 02:29:48 -0500

    FIX: validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+    before_validation :set_columns
+    validates :name, presence: {message: 'El producto debe tener un nombre'}
+    def set_columns
+        self.price ||= 0
+        self.cost ||= 0
+        self.quantity ||= 1
+    end
 end

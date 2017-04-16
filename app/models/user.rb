33dd1f58c5e49f51cfd7ba7894dc718e52447a10
Fabian Pierre Peña Jacobo <f.pena.jacobo@gmail.com>
2017-04-15 22:15:24 -0500

    ADD: register functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  validates :email, uniqueness: {message: 'El email ya está en uso'}
 end
